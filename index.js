6505c23a17ccfce021bfb4a59aafb1da079f566b
swiing <54545719+swiing@users.noreply.github.com>
2025-05-07 14:44:57 +0200

    Use codename for host
    This is temporary until final name is given.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export const PROJECT = "Stockastix"
-export const HOSTNAME = `${PROJECT.toLowerCase()}.com`
+export const HOSTNAME = `parthenon-${PROJECT.toLowerCase()}.com`
 export const URL = `https://www.${HOSTNAME}`
